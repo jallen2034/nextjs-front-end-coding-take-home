84df75ebae7024b7ae311db883126cdc0b7040a6
Jacob (╯°o°）╯︵ ┻━┻ <jacob@otonomos.com>
2024-10-07 19:08:30 -0700

    Added functionality to read data from our CSV server side then display it in a client sode react compoennt

---
+++ src/app/map/types.ts
@@ -0,0 +1,20 @@
+export interface CsvParseOptions {
+  columns: boolean; // Parse CSV into objects with headers as keys
+  skip_empty_lines: boolean; // Skip empty lines
+  delimiter: string; // Field delimiter (e.g., ',')
+  trim: boolean; // Trim whitespace around fields
+}
+
+export interface Property {
+  id: string;
+  area_sqft: string;
+  bedrooms: string;
+  bathrooms: string;
+  price: string;
+  date: string;
+  address: string;
+  latitude: string;
+  longitude: string;
+}
+
+export type ResaleData = Property[];
