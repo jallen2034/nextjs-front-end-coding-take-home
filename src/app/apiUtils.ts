a57f90e11b225bd9aeb8c16cf1eb28603342d2a2
Jacob (╯°o°）╯︵ ┻━┻ <jacob@otonomos.com>
2024-10-07 18:24:00 -0700

    Added the react-map-gl dependency to the project and built a basic component for this that can be re used

---
+++ src/app/apiUtils.ts
@@ -0,0 +1,5 @@
+const MAPBOX_API_SECRET_KEY: string = process.env.NEXT_PUBLIC_MAPBOX_API_KEY || '';
+
+export {
+  MAPBOX_API_SECRET_KEY
+};
--- src/app/map/page.tsx
+++ src/app/map/page.tsx
@@ -1,10 +1,14 @@
 import React from "react";
+import MapboxContainer from "@/app/shared-components/react-mapbox/mapbox-container"
 
 // Default export for the map page
 const MapPage = () => {
   return (
     <div className="map-main-container">
       Hello, I will be the map page :)
+      <div>
+        <MapboxContainer/>
+      </div>
     </div>
   );
 };
