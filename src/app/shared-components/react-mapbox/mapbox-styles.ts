95003150efc53adf9d6a9ccdb9a2b3d6e909e742
Jacob (╯°o°）╯︵ ┻━┻ <jacob@otonomos.com>
2024-10-10 16:05:02 -0700

    Added marker clustering, also updated README with API key setup instructions

---
+++ src/app/shared-components/react-mapbox/mapbox-styles.ts
@@ -0,0 +1,55 @@
+// Cluster Layer - https://github.com/visgl/react-map-gl/blob/7.1-release/examples/clusters/src/layers.ts
+import { CircleLayer } from "mapbox-gl";
+import { LayerProps } from "react-map-gl";
+
+const clusterLayer: CircleLayer = {
+  id: "clusters",
+  type: "circle",
+  source: "records",
+  filter: ["has", "point_count"],
+  paint: {
+    "circle-color": [
+      "step",
+      ["get", "point_count"],
+      "#733bc4",
+      100,
+      "#ce175f",
+      750,
+      "#196cc9",
+    ],
+    "circle-radius": ["step", ["get", "point_count"], 20, 100, 30, 750, 40],
+  },
+};
+
+// Cluster Count Layer - https://github.com/visgl/react-map-gl/blob/7.1-release/examples/clusters/src/layers.ts
+const clusterCountLayer: LayerProps = {
+  id: "cluster-count",
+  type: "symbol",
+  source: "records",
+  filter: ["has", "point_count"],
+  layout: {
+    "text-field": "{point_count_abbreviated}",
+    "text-font": ["DIN Offc Pro Medium", "Arial Unicode MS Bold"],
+    "text-size": 12,
+  },
+};
+
+// Unclustered Point Layer.
+const unclusteredPointLayer: CircleLayer = {
+  id: "unclustered-point",
+  type: "circle",
+  source: "records",
+  filter: ["!", ["has", "point_count"]],
+  paint: {
+    "circle-color": "#4e06bb",
+    "circle-radius": 4,
+    "circle-stroke-width": 1,
+    "circle-stroke-color": "#fff",
+  },
+};
+
+export {
+  unclusteredPointLayer,
+  clusterCountLayer,
+  clusterLayer
+}
